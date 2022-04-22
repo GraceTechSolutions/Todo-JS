559170d7cf30781271441b2602572f36986072db
JRudransh <JRudransh@protonmail.com>
2022-04-22 17:01:42 +0530

    Added TODO

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
 const input = document.getElementById('input')
 const submit = document.getElementById('submit')
 const child = document.getElementsByClassName('child')[0]
+const todoForm = document.getElementById('todoForm')
 
 
+function setTodo(todo) {
+    perviousTodo = localStorage.getItem('todo')
+    if (perviousTodo) {
+        localStorage.setItem('todo', `${todo}, ${perviousTodo}`)
+    } else {
+        localStorage.setItem('todo', `${todo}`)
+    }
+}
 
-submit.addEventListener('click', () => {
+function displayTodo(value){
     let element = document.createElement('div')
     element.classList.add('todo')
-    element.innerHTML = `<p>${input.value}</p>`
+    element.innerHTML = `<p>${value}</p>`
     child.appendChild(element)
-    
-    console.log(input.value);
+}
+
+function initTodo(){
+    data = localStorage.getItem('todo')
+    if (data) {
+        dataArr = data.split(', ')
+        console.log(dataArr);
+        dataArr.forEach(dataTodo => {
+            displayTodo(dataTodo)
+        })
+    }
+}
+
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (input.value) {
+        setTodo(input.value)
+        child.innerHTML = ''
+        initTodo()
+        input.value = ''
+    }
 })
+
+initTodo()
